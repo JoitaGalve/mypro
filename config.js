84e88a17808e4644caaa043dcb35f088067db2dd
JoitaGalve <79201954+JoitaGalve@users.noreply.github.com>
2021-11-28 19:06:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkaGKCjgCqHD_HI46TF7P6b_4CNB1nabw",
+  authDomain: "storytellingapp-b4096.firebaseapp.com",
+  projectId: "storytellingapp-b4096",
+  storageBucket: "storytellingapp-b4096.appspot.com",
+  messagingSenderId: "939297620962",
+  appId: "1:939297620962:web:46d4015a08f8300f2aefbb"
+};
